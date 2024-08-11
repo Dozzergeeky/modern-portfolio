71dd2a997a7afe6d8105c7a50fb3de4ef6c2ce68
Dozzergeeky <84225812+Dozzergeeky@users.noreply.github.com>
2024-08-11 21:51:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ data/investments.js
@@ -0,0 +1,58 @@
+const investments = [
+  {
+    title: 'Dub',
+    description: 'Link management infrastructure.',
+    icon: 'dub',
+    url: 'https://dub.co',
+  },
+  {
+    title: 'Langbase',
+    description: 'Open-source OpenAI alternative.',
+    icon: 'langbase',
+    url: 'https://langbase.com',
+  },
+  {
+    title: 'Vite',
+    description: 'Tooling for next generation frontend.',
+    icon: 'vite',
+    url: 'https://vitejs.dev',
+  },
+  {
+    title: 'Mergent',
+    description: 'Providing schedulers in a reliable way.',
+    icon: 'mergent',
+    url: 'https://mergent.co',
+  },
+  {
+    title: 'Charm',
+    description: 'Making the command line glamorous.',
+    icon: 'charm',
+    url: 'https://charm.sh',
+  },
+  {
+    title: 'Fig',
+    description: 'Reimagining your existing terminal experience.',
+    icon: 'fig',
+    url: 'https://fig.io',
+  },
+  {
+    title: 'Koala',
+    description: 'Finding qualified leads with data science.',
+    icon: 'koala',
+    url: 'https://getkoala.com',
+  },
+  {
+    title: 'Customer.io',
+    description: 'Automating email messages and workflows.',
+    icon: 'customer-io',
+    url: 'https://customer.io',
+  },
+  {
+    title: 'Gumroad',
+    description: 'Making it easy to earn your first dollar online.',
+    icon: 'gumroad',
+    url: 'https://gumroad.com',
+  },
+]
+
+export default investments
